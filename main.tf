28ca702d45ad8517724e1dc99ed98a695a1b4767
Bo Pang <53412945+BoPang123@users.noreply.github.com>
2023-10-18 17:38:46 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,10 @@
-resource "aws_security_group" "example" {
-  name        = "example"
-  description = "Example security group"
-
-  # 添加允许所有流量的入站规则，包括8080端口
-  ingress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-
 
 resource "aws_instance" "my_vm" {
  ami           = var.ami //Ubuntu AMI
  instance_type = var.instance_type
-vpc_security_group_ids = [aws_security_group.example.id]
+key_name      = "Oasis.pem" 
+security_groups ="oasis-jenkins-4"
+#vpc_security_group_ids = [aws_security_group.example.id]
  tags = {
    Name = var.name_tag,
  }
