0151cdd43f0b3de0419deee2688b9b42ea017ef6
Bo Pang <53412945+BoPang123@users.noreply.github.com>
2023-10-19 17:28:30 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,8 +16,8 @@ security_groups =["oasis-jenkins-4"]
               sudo systemctl start docker
               sudo systemctl enable docker
               sudo usermod -a -G docker ubuntu
-              sudo docker pull 123banzhang/nus_dmss_practicemodule_team3:1.0 # 拉取 Docker 镜像,仅修改“:”后面的版本号
-              sudo docker run -d -p 8080:8080 123banzhang/nus_dmss_practicemodule_team3:0.1.0-SNAPSHOT # 运行容器
+              sudo docker pull 123banzhang/nus_dmss_practicemodule_team3:1.0 # 拉取 Docker 镜像,这里要修改image版本号！仅修改“:”后面的版本号
+              sudo docker run -d -p 8080:8080 123banzhang/nus_dmss_practicemodule_team3:1.0 # 运行容器，这里要修改image版本号！仅修改“:”后面的版本号
               EOF
 }
 
